5c6ec6460ec55d3bfef65884061a716ab0acb22f
AdamConverse <adam.converse@gmail.com>
2016-11-21 20:49:48 -0600

    update positions

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+var bodyParser = require('body-parser')
+var app = express()
+var location_x = 0;
+var location_y = 0;
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -12,10 +16,26 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('It works!');
+});
+
+app.post('/', function (req, res) {
+  location_x = req.body.x_pos;
+  location_y = req.body.y_pos;
+  res.send('Success!');
+});
+
+app.listen(5000, function () {
+  console.log('API is listening on port 5000!');
+});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('location', location_x, location_y), 500);
