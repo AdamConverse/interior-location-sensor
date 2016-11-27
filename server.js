5c6f1cdbab209e1910a6eb27270528956b52e9f2
Adam Converse <adam.converse@gmail.com>
2016-11-27 17:15:36 -0600

    update for one port

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
-'use strict';
-
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var bodyParser = require('body-parser')
-var app = express()
-var location_x = 0;
-var location_y = 0;
 
-const PORT = process.env.PORT || 3000;
-const API_PORT = process.env.PORT || 5000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
+app.use(express.static(__dirname + '/bower_components'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('It works!');
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', function (req, res) {
+const PORT = process.env.PORT || 3000;
+
+app.post('/', function (req, res,next) {
   location_x = req.body.x_pos;
   location_y = req.body.y_pos;
+  io.emit('location', location_x, location_y);
   res.send('Success!');
 });
 
-app.listen(API_PORT, function () {
-  console.log('API is listening on port ' + API_PORT);
-});
+server.listen(PORT);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+io.on('connection', function(client) {
+    console.log('Client connected...');
 
-setInterval(() => io.emit('location', location_x, location_y), 500);
+    client.on('location', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+
+});
