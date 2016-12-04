7a5a7b23310395a0bdfdf274b0495b6d6f29a087
AdamConverse <adam.converse@gmail.com>
2016-12-04 17:32:10 -0600

    remove old nodes

--- arduino_serial/serial_to_http.py
+++ arduino_serial/serial_to_http.py
@@ -10,12 +10,13 @@ serial_location = '/dev/cu.usbmodem1421'
 ser_in = serial.Serial(serial_location, baud_rate)
 
 if __name__ == '__main__':
+    i = 0
     while True:
         data = ser_in.readline()[:-1].split(",")
-        print data
+        if i%50 is 0:
+            payload = {'rinches': str(data[0]), 'linches': str(data[1]), 'angle': str(data[2])}
 
-        payload = {'rinches': str(data[0]), 'linches': str(data[1]), 'angle': str(data[2])}
-
-        r = requests.post("https://serene-reaches-34295.herokuapp.com/",
-                data = json.dumps(payload),
-                headers = {'Content-type': 'application/json'})
+            r = requests.post("https://serene-reaches-34295.herokuapp.com/arduino-1/",
+                    data = json.dumps(payload),
+                    headers = {'Content-type': 'application/json'})
+        i = i + 1
