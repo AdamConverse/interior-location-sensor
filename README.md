2f2d27054e6e94f3792e1f1b0346295e106c3954
AdamConverse <adam.converse@gmail.com>
2016-12-05 19:52:25 -0600

    update grid size

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Interior Location Sensor Web App
+Project Goal: Track a persons location in a 10' x 10' without a camera or any additional sensors on the person.
 
-## Sample Request
-```json
-{
-  "x_pos":"1",
-  "y_pos":"2"
-}
-```
+* [Link to Web App](https://serene-reaches-34295.herokuapp.com/)
+* [Link to Arduino Repo](https://github.com/csgreen3/iot_interior_location/blob/master/distanceAngle/distanceAngle.ino)
--- arduino_serial/serial_to_http.py
+++ arduino_serial/serial_to_http.py
@@ -8,14 +8,14 @@ baud_rate = 9600
 serial_location = '/dev/cu.usbmodem1421'
 
 ser_in = serial.Serial(serial_location, baud_rate)
+s = requests.Session()
 
 if __name__ == '__main__':
     i = 0
     while True:
         data = ser_in.readline()[:-1].split(",")
-        if i%25 is 0:
-            payload = {'rinches': str(data[0]), 'linches': str(data[1]), 'angle': str(data[2])}
-            r = requests.post("https://serene-reaches-34295.herokuapp.com/arduino-1/",
-                    data = json.dumps(payload),
-                    headers = {'Content-type': 'application/json'})
+        payload = {'rinches': str(data[0]), 'linches': str(data[1]), 'angle': str(data[2])}
+        r = s.post("https://serene-reaches-34295.herokuapp.com/arduino-1/",
+                data = json.dumps(payload),
+                headers = {'Content-type': 'application/json'})
         i = i + 1
